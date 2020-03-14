8a0bf9c5eaeab41da67f952626f41591ac70e61c
xiraynedev <xiraynedev@gmail.com>
2020-03-14 16:54:30 -0400

    Updates

---
+++ precache-manifest.6d0edb8f391facefd94702abd617bc94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff0f20f12ca9585bb2e0b5f3a7d540b2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "102515b7d14510e412e0",
+    "url": "/monsters-rolodex/static/css/main.77538878.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "102515b7d14510e412e0",
+    "url": "/monsters-rolodex/static/js/main.b68c9459.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
